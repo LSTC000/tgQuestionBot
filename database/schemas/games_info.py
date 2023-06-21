c93f4a9b376d7c2b09a186c2bc8f3c3c849c36b1
Ivan <razgonyaev_74@mail.ru>
2023-06-21 13:46:54 +0700

    add games_info.py schema

---
+++ database/schemas/games_info.py
@@ -0,0 +1,36 @@
+import datetime
+
+from database.database_setup import BaseModel
+
+from sqlalchemy import Column, BigInteger, VARCHAR, Integer, DateTime, sql, func
+
+
+class GamesInfo(BaseModel):
+    __tablename__ = 'games_info'
+
+    # Auto increment id.
+    id = Column(BigInteger, primary_key=True, autoincrement=True,
+                server_default=sql.text('nextval(\'games_info_id_seq\')'))
+    # Game id.
+    game_id = Column(Integer, nullable=False)
+    # Game name.
+    game_name = Column(VARCHAR(64), nullable=True)
+    # Number of attempts.
+    attempts = Column(Integer, nullable=False)
+    # Number of completed attempts.
+    completed_attempts = Column(Integer, nullable=False)
+    # Game likes.
+    likes = Column(Integer, nullable=False)
+    # Game dislikes.
+    dislikes = Column(Integer, nullable=False)
+    # Created game date.
+    created_date = Column(DateTime(True), server_default=func.now())
+    # Update game date.
+    updated_date = Column(
+        DateTime(True),
+        default=datetime.datetime.utcnow,
+        onupdate=datetime.datetime.utcnow,
+        server_default=func.now()
+    )
+
+    query: sql.select

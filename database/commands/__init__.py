9f75e1dbcf1e6e3c45840ca545b24922e6758243
Ivan <razgonyaev_74@mail.ru>
2023-06-21 14:24:49 +0700

    add delete_user_alert.py database command

--- database/commands/__init__.py
+++ database/commands/__init__.py
@@ -1,7 +1,9 @@
 __all__ = [
     'get_users_alert',
-    'add_user_alert'
+    'add_user_alert',
+    'delete_user_alert'
 ]
 
 from .get_users_alert import get_users_alert
 from .add_user_alert import add_user_alert
+from .delete_user_alert import delete_user_alert

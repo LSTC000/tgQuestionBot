c0f379dba6d4934870b701a116a90a6693d308b8
Ivan <razgonyaev_74@mail.ru>
2023-06-21 20:16:14 +0700

    update start_msg.py

--- handlers/users/commands/start_msg.py
+++ handlers/users/commands/start_msg.py
@@ -4,7 +4,9 @@ from data.redis import LAST_IKB_REDIS_KEY
 
 from data.messages import START_MESSAGE
 
-from functions import ikb_clear
+from database import add_user_info
+
+from functions import ikb_clear, check_user_info_cache
 
 from keyboards import start_ikb
 
@@ -18,6 +20,9 @@ async def start_msg(message: types.Message, state: FSMContext) -> None:
 
     # Clear previous inline keyboard.
     await ikb_clear(user_id=user_id, state=state)
+    # We check whether there is information about the user in the database, if not, then add it there.
+    if not await check_user_info_cache(user_id):
+        await add_user_info(user_id, message.from_user.first_name)
 
     async with state.proxy() as data:
         # Clear all redis data for user.

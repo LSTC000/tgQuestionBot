b8ba9e5f2e455d16f17232a8df76704f24770cc5
Ivan <razgonyaev_74@mail.ru>
2023-06-21 19:07:58 +0700

    update start_msg.py

--- handlers/users/commands/start_msg.py
+++ handlers/users/commands/start_msg.py
@@ -4,6 +4,10 @@ from data.redis import LAST_IKB_REDIS_KEY
 
 from data.messages import START_MESSAGE
 
+from functions import ikb_clear
+
+from keyboards import start_ikb
+
 from aiogram import types
 from aiogram.dispatcher.storage import FSMContext
 
@@ -12,12 +16,17 @@ from aiogram.dispatcher.storage import FSMContext
 async def start_msg(message: types.Message, state: FSMContext) -> None:
     user_id = message.from_user.id
 
-    async with state.proxy() as data:
-        data.clear()
+    # Clear previous inline keyboard.
+    await ikb_clear(user_id=user_id, state=state)
 
+    async with state.proxy() as data:
+        # Clear all redis data for user.
+        data.clear()
+        # Call start inline menu.
         msg = await bot.send_message(
             chat_id=user_id,
-            text=START_MESSAGE.format(message.from_user.first_name)
+            text=START_MESSAGE.format(message.from_user.first_name),
+            reply_markup=start_ikb()
         )
-
+        # Remember id of the last inline keyboard.
         data[LAST_IKB_REDIS_KEY] = msg.message_id

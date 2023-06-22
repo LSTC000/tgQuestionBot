fa2c3934a924f2e1e46f4ac187734759ae3d2594
Ivan <razgonyaev_74@mail.ru>
2023-06-22 16:37:02 +0700

    add edit_main_menu_ikb.py

--- functions/handlers/__init__.py
+++ functions/handlers/__init__.py
@@ -1,6 +1,12 @@
-__all__ = ['last_ikb_clear', 'call_start_ikb', 'call_main_menu_ikb']
+__all__ = [
+    'last_ikb_clear',
+    'call_start_ikb',
+    'call_main_menu_ikb',
+    'edit_main_menu_ikb'
+]
 
 
 from .last_ikb_clear import last_ikb_clear
 from .call_start_ikb import call_start_ikb
 from .call_main_menu_ikb import call_main_menu_ikb
+from .edit_main_menu_ikb import edit_main_menu_ikb

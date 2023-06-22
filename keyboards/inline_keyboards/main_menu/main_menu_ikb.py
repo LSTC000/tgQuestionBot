e0159c3cea63c238965e61bfd7cff2dc15931c99
Ivan <razgonyaev_74@mail.ru>
2023-06-22 18:36:22 +0700

    update main_menu_ikb.py

--- keyboards/inline_keyboards/main_menu/main_menu_ikb.py
+++ keyboards/inline_keyboards/main_menu/main_menu_ikb.py
@@ -25,7 +25,7 @@ def main_menu_ikb(alert: bool) -> InlineKeyboardMarkup:
     ikb = InlineKeyboardMarkup(row_width=ROW_WIDTH)
 
     ikb.row(InlineKeyboardButton(text=GAMES_PICKER_IKB_MESSAGE, callback_data=GAMES_PICKER_CALLBACK_DATA))
-    ikb.row(InlineKeyboardButton(text=TESTS_PICKER_CALLBACK_DATA, callback_data=TESTS_PICKER_IKB_MESSAGE))
+    ikb.row(InlineKeyboardButton(text=TESTS_PICKER_IKB_MESSAGE, callback_data=TESTS_PICKER_CALLBACK_DATA))
     ikb.row(InlineKeyboardButton(
         text=USER_ALERT_OFF_IKB_MESSAGE if alert else USER_ALERT_ON_IKB_MESSAGE,
         callback_data=CHANGE_USER_ALERT_CALLBACK_DATA)

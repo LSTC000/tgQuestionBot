1389f7647eb4417ec61150f2044abfdaecb723cd
Ivan <razgonyaev_74@mail.ru>
2023-06-22 15:40:49 +0700

    add check_user_alert_cache.py

--- functions/cache/__init__.py
+++ functions/cache/__init__.py
@@ -1,5 +1,8 @@
 __all__ = [
-    'check_user_info_cache'
+    'check_user_info_cache',
+    'check_user_alert_cache'
 ]
 
+
 from .check_user_info_cache import check_user_info_cache
+from .check_user_alert_cache import check_user_alert_cache

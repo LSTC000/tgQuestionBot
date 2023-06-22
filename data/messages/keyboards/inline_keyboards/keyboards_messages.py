0c81ec1d6829b5e2d647f9fdfdf45c1bbcd7cc9f
Ivan <razgonyaev_74@mail.ru>
2023-06-22 18:34:56 +0700

    update TESTS_PICKER_IKB_MESSAGE

--- data/messages/keyboards/inline_keyboards/keyboards_messages.py
+++ data/messages/keyboards/inline_keyboards/keyboards_messages.py
@@ -4,6 +4,6 @@ CANCEL_TO_MAIN_MENU_IKB_MESSAGE = '⬅️ Вернуться в главное �
 START_IKB_MESSAGE = 'Давай начнём 🚀'
 # MAIN MENU.
 GAMES_PICKER_IKB_MESSAGE = '🎲 Выберите игру'
-TESTS_PICKER_IKB_MESSAGE = 'Выберите тест'
+TESTS_PICKER_IKB_MESSAGE = '📕 Выберите тест'
 USER_ALERT_ON_IKB_MESSAGE = '🔔 Включить уведомления'
 USER_ALERT_OFF_IKB_MESSAGE = '🔕 Отключить уведомления'

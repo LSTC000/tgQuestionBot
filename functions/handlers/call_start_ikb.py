59217bca1f985be271f5a8a9652a5fd689db5521
Ivan <razgonyaev_74@mail.ru>
2023-06-22 15:18:18 +0700

    update call_start_ikb.py

--- functions/handlers/call_start_ikb.py
+++ functions/handlers/call_start_ikb.py
@@ -2,17 +2,17 @@ from data.redis import LAST_IKB_REDIS_KEY
 
 from data.messages import START_MESSAGE
 
+from keyboards import start_ikb
+
 from loader import bot
 
 from aiogram.dispatcher.storage import FSMContext
-from aiogram.types import InlineKeyboardMarkup
 
 
-async def call_start_ikb(user_id: int, first_name: str, ikb: InlineKeyboardMarkup, state: FSMContext) -> None:
+async def call_start_ikb(user_id: int, first_name: str, state: FSMContext) -> None:
     '''
     :param user_id: Telegram user id.
     :param first_name: Telegram user first name.
-    :param ikb: Inline keyboard.
     :param state: FSMContext.
     :return: None.
     '''
@@ -22,7 +22,7 @@ async def call_start_ikb(user_id: int, first_name: str, ikb: InlineKeyboardMarku
         msg = await bot.send_message(
             chat_id=user_id,
             text=START_MESSAGE.format(first_name),
-            reply_markup=ikb()
+            reply_markup=start_ikb()
         )
         # Remember id of the last inline keyboard.
         data[LAST_IKB_REDIS_KEY] = msg.message_id

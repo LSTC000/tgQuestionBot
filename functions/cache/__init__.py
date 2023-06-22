fade1d5d9585441e33d0b0315007c1f9fd0fd6a0
Ivan <razgonyaev_74@mail.ru>
2023-06-22 15:48:53 +0700

    add delete_user_alert_cache.py

--- functions/cache/__init__.py
+++ functions/cache/__init__.py
@@ -1,12 +1,14 @@
 __all__ = [
     'check_user_info_cache',
     'check_user_alert_cache',
-    'add_user_alert_cache'
+    'add_user_alert_cache',
+    'delete_user_alert_cache'
 ]
 
 
 # Alerts.
 from .check_user_alert_cache import check_user_alert_cache
 from .add_user_alert_cache import add_user_alert_cache
+from .delete_user_alert_cache import delete_user_alert_cache
 # UsersInfo.
 from .check_user_info_cache import check_user_info_cache

700c09eb0349489c95c0791950c8eda71c480d40
Ivan <razgonyaev_74@mail.ru>
2023-06-23 15:27:52 +0700

    update naruto_config.py

--- data/config/questions/games/naruto_config.py
+++ data/config/questions/games/naruto_config.py
@@ -1,13 +1,13 @@
 NARUTO_NAME = 'Naruto'
 NARUTO_DATA = {
-    1: {
+    0: {
         'image_bytes': None,
         'image_url': None,
         'image_path': None,
         'question': 'Вопрос 1',
         'answers': ['A', 'B', 'C', 'D']
     },
-    2: {
+    1: {
             'image_bytes': None,
             'image_url': None,
             'image_path': None,

e8917a69c79b97d56d6d3b8323c477fb252a10bd
Ivan <razgonyaev_74@mail.ru>
2023-06-23 15:24:37 +0700

    add naruto_config.py

--- data/config/questions/games/__init__.py
+++ data/config/questions/games/__init__.py
@@ -1,3 +1,10 @@
-__all__ = ['GAMES_DATA', 'GAMES_NAME', 'GAMES_COUNT']
+__all__ = [
+    'GAMES_DATA',
+    'GAMES_NAME',
+    'GAMES_COUNT',
+    'NARUTO_DATA',
+    'NARUTO_NAME'
+]
 
 from .games_config import GAMES_DATA, GAMES_NAME, GAMES_COUNT
+from .naruto_config import NARUTO_DATA, NARUTO_NAME

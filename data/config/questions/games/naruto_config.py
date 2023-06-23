171765460dd5fd33df9a27f5506df603d4647da8
Ivan <razgonyaev_74@mail.ru>
2023-06-23 15:40:52 +0700

    update naruto_config.py

--- data/config/questions/games/naruto_config.py
+++ data/config/questions/games/naruto_config.py
@@ -1,17 +1,20 @@
+from data.paths import NARUTO_IMAGE_PATH_0
+
+
 NARUTO_NAME = 'Naruto'
 NARUTO_DATA = {
     0: {
-        'image_bytes': None,
-        'image_url': None,
-        'image_path': None,
+        'image': {
+            'path': NARUTO_IMAGE_PATH_0,
+            'bytes': None,
+            'url': None
+        },
         'question': 'Вопрос 1',
         'answers': ['A', 'B', 'C', 'D']
     },
     1: {
-            'image_bytes': None,
-            'image_url': None,
-            'image_path': None,
-            'question': 'Вопрос 2',
-            'answers': ['A', 'B', 'C', 'D']
-        }
+        'image': None,
+        'question': 'Вопрос 2',
+        'answers': ['A', 'B', 'C', 'D']
+    }
 }

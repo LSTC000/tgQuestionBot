3d1b661843e4baf0a6c5a115eb9513295817cf12
Ivan <razgonyaev_74@mail.ru>
2023-06-23 13:09:14 +0700

    add questions redis keys

--- data/redis/redis_keys.py
+++ data/redis/redis_keys.py
@@ -2,3 +2,8 @@
 LAST_IKB_REDIS_KEY = 'last_ikb'
 # PICKERS.
 PICKER_PAGE_REDIS_KEY = 'picker_page'
+# QUESTIONS.
+GAME_NAME_REDIS_KEY = 'game_name'
+GAME_QUESTION_REDIS_KEY = 'game_question'
+TEST_NAME_REDIS_KEY = 'test_name'
+TEST_QUESTION_REDIS_KEY = 'test_question'

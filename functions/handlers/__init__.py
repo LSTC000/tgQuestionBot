5d505f8e2ad53f46b63e3f4003c2e76c1ef77398
Ivan <razgonyaev_74@mail.ru>
2023-06-23 13:15:29 +0700

    add clear_redis_data.py

--- functions/handlers/__init__.py
+++ functions/handlers/__init__.py
@@ -1,16 +1,18 @@
 __all__ = [
-    'clear_last_ikb',
     'call_start_ikb',
     'call_main_menu_ikb',
     'call_games_picker_ikb',
     'call_tests_picker_ikb',
-    'edit_main_menu_ikb'
+    'edit_main_menu_ikb',
+    'clear_last_ikb',
+    'clear_redis_data'
 ]
 
 
-from .clear_last_ikb import clear_last_ikb
 from .call_start_ikb import call_start_ikb
 from .call_main_menu_ikb import call_main_menu_ikb
 from .call_games_picker_ikb import call_games_picker_ikb
 from .call_tests_picker_ikb import call_tests_picker_ikb
+from .clear_last_ikb import clear_last_ikb
+from .clear_redis_data import clear_redis_data
 from .edit_main_menu_ikb import edit_main_menu_ikb
---
+++ functions/handlers/clear_redis_data.py
@@ -0,0 +1,32 @@
+from data.redis import (
+    PICKER_PAGE_REDIS_KEY,
+    GAME_NAME_REDIS_KEY,
+    GAME_QUESTION_REDIS_KEY,
+    TEST_NAME_REDIS_KEY,
+    TEST_QUESTION_REDIS_KEY
+)
+
+from aiogram.dispatcher.storage import FSMContext
+
+
+async def clear_redis_data(state: FSMContext) -> None:
+    '''
+    :param state: FSMContext.
+    :return: None.
+    '''
+
+    async with state.proxy() as data:
+        if PICKER_PAGE_REDIS_KEY in data:
+            data.pop(PICKER_PAGE_REDIS_KEY)
+
+        if GAME_NAME_REDIS_KEY in data:
+            data.pop(GAME_NAME_REDIS_KEY)
+
+        if GAME_QUESTION_REDIS_KEY in data:
+            data.pop(GAME_QUESTION_REDIS_KEY)
+
+        if TEST_NAME_REDIS_KEY in data:
+            data.pop(TEST_NAME_REDIS_KEY)
+
+        if TEST_QUESTION_REDIS_KEY in data:
+            data.pop(TEST_QUESTION_REDIS_KEY)

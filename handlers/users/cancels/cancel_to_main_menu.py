c4da8ff29b8134df4edf9bb3eb5164ec7ba04c0d
Ivan <razgonyaev_74@mail.ru>
2023-06-24 12:17:17 +0700

    update cancel_to_main_menu.py

--- handlers/users/cancels/cancel_to_main_menu.py
+++ handlers/users/cancels/cancel_to_main_menu.py
@@ -2,7 +2,7 @@ from loader import dp
 
 from data.callbacks import CANCEL_TO_MAIN_MENU_CALLBACK_DATA
 
-from functions import clear_last_ikb, call_main_menu_ikb, check_user_alert_cache
+from functions import clear_last_ikb, clear_redis_data, call_main_menu_ikb, check_user_alert_cache
 
 from states import MainMenuStatesGroup, PickersStatesGroup, GamesStatesGroup
 
@@ -22,6 +22,8 @@ from aiogram.dispatcher.storage import FSMContext
 async def cancel_to_main_menu(callback: types.CallbackQuery, state: FSMContext) -> None:
     user_id = callback.from_user.id
 
+    # Clear redis data.
+    await clear_redis_data(state=state)
     # Clear last inline keyboard.
     await clear_last_ikb(user_id=user_id, state=state)
     # Call main inline menu.

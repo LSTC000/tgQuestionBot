573f6e93d4dba89d4fc013123e6d8b27e0ed4a65
Ivan <razgonyaev_74@mail.ru>
2023-06-25 22:45:18 +0700

    add call_questions_creator.py

--- functions/handlers/__init__.py
+++ functions/handlers/__init__.py
@@ -3,16 +3,23 @@ __all__ = [
     'call_main_menu_ikb',
     'call_games_picker_ikb',
     'call_tests_picker_ikb',
+    'call_questions_creator',
     'edit_main_menu_ikb',
     'clear_last_ikb',
-    'clear_redis_data'
+    'clear_redis_data',
 ]
 
 
+# CALL INLINE KEYBOARDS.
 from .call_start_ikb import call_start_ikb
 from .call_main_menu_ikb import call_main_menu_ikb
+# CALL INLINE PICKERS.
 from .call_games_picker_ikb import call_games_picker_ikb
 from .call_tests_picker_ikb import call_tests_picker_ikb
+# CALL CREATORS.
+from .call_questions_creator import call_questions_creator
+# CLEARS.
 from .clear_last_ikb import clear_last_ikb
 from .clear_redis_data import clear_redis_data
+# EDITS.
 from .edit_main_menu_ikb import edit_main_menu_ikb
---
+++ functions/handlers/call_questions_creator.py
@@ -0,0 +1,35 @@
+from data.redis import (
+    GAME_QUESTION_REDIS_KEY,
+    GAME_QUESTION_NUMBER_REDIS_KEY,
+    LAST_IKB_REDIS_KEY,
+)
+
+from creators import GamesCreator
+
+from loader import bot
+
+from aiogram.dispatcher.storage import FSMContext
+
+
+async def call_questions_creator(user_id: int, state: FSMContext) -> None:
+    """
+    :param user_id: Telegram user id.
+    :param state: FSMContext.
+    :return: None.
+    """
+
+    async with state.proxy() as data:
+        # Get image, question and questions inline keyboard.
+        image, question, ikb = GamesCreator().questions_creator(state)
+        # Call questions creator.
+        if image is not None:
+            msg = await bot.send_photo(chat_id=user_id, caption=question, photo=image, reply_markup=ikb)
+        else:
+            msg = await bot.send_message(chat_id=user_id, text=question, reply_markup=ikb)
+        # Move on to the next question.
+        data[GAME_QUESTION_NUMBER_REDIS_KEY] += 1
+        # Remember question.
+        data[GAME_QUESTION_REDIS_KEY] = question
+        # Remember id of the last inline keyboard.
+        data[LAST_IKB_REDIS_KEY] = msg.message_id
+

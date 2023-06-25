fb850d4bd455c4424a64964d1fb08aff4575c251
Ivan <razgonyaev_74@mail.ru>
2023-06-25 22:45:04 +0700

    update games_creator.py

--- creators/games_creator.py
+++ creators/games_creator.py
@@ -1,38 +1,36 @@
 import os
 
-from loader import bot
-
 from data.callbacks import CANCEL_TO_MAIN_MENU_CALLBACK_DATA
 
 from data.messages import CANCEL_TO_MAIN_MENU_IKB_MESSAGE
 
 from data.config import GAMES_DATA
 
-from data.redis import GAME_NAME_REDIS_KEY, GAME_QUESTION_NUMBER_REDIS_KEY, LAST_IKB_REDIS_KEY
+from data.redis import GAME_NAME_REDIS_KEY, GAME_QUESTION_NUMBER_REDIS_KEY
 
 from aiogram.dispatcher.storage import FSMContext
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputFile
 
 
 class GamesCreator:
-    async def questions_creator(self, user_id: int, state: FSMContext) -> None:
+    async def questions_creator(self, state: FSMContext) -> tuple:
         """
-        :param user_id: Telegram user id.
         :param state: FSMContext.
-        :return: None.
+        :return: Question image, question data (text) and questions inline keyboard.
         """
 
         async with state.proxy() as data:
             game_name = data[GAME_NAME_REDIS_KEY]
-            game_question = data[GAME_QUESTION_NUMBER_REDIS_KEY]
-            data[GAME_QUESTION_NUMBER_REDIS_KEY] += 1
+            question_number = data[GAME_QUESTION_NUMBER_REDIS_KEY]
 
         ikb = InlineKeyboardMarkup()
 
-        question_data = GAMES_DATA[game_name][game_question]
+        game_data = GAMES_DATA[game_name]
+        question_data = game_data[question_number]
 
         image = question_data['image']
         answers = question_data['answers']
+        question = question_data['question']
 
         if image is not None:
             for key in image.keys():
@@ -50,19 +48,6 @@ class GamesCreator:
             InlineKeyboardButton(text=CANCEL_TO_MAIN_MENU_IKB_MESSAGE, callback_data=CANCEL_TO_MAIN_MENU_CALLBACK_DATA)
         )
 
-        if image is not None:
-            msg = await bot.send_photo(
-                chat_id=user_id,
-                caption=question_data['question'],
-                photo=image,
-                reply_markup=ikb
-            )
-        else:
-            msg = await bot.send_message(
-                chat_id=user_id,
-                text=question_data['question'],
-                reply_markup=ikb
-            )
+        question = f'Вопрос {question_number + 1}/{len(GAMES_DATA[game_data])}\n\n{question}'
 
-        async with state.proxy() as data:
-            data[LAST_IKB_REDIS_KEY] = msg.message_id
+        return image, question, ikb

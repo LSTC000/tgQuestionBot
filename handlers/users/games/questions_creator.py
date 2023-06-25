07be2688a768d893984ec08ec44264a0c1b94b07
Ivan <razgonyaev_74@mail.ru>
2023-06-25 22:54:51 +0700

    update questions_creator.py

--- handlers/users/games/questions_creator.py
+++ handlers/users/games/questions_creator.py
@@ -2,11 +2,16 @@ from loader import dp
 
 from data.callbacks import CANCEL_TO_MAIN_MENU_CALLBACK_DATA
 
-from data.redis import USER_ANSWERS_REDIS_KEY
+from data.config import GAMES_DATA
 
-from creators import GamesCreator
+from data.redis import (
+    USER_ANSWERS_REDIS_KEY,
+    GAME_QUESTION_REDIS_KEY,
+    GAME_QUESTION_NUMBER_REDIS_KEY,
+    GAME_NAME_REDIS_KEY
+)
 
-from functions import clear_last_ikb
+from functions import clear_last_ikb, call_questions_creator
 
 from states import GamesStatesGroup
 
@@ -18,10 +23,17 @@ from aiogram.dispatcher.storage import FSMContext
 async def questions_creator(callback: types.CallbackQuery, state: FSMContext) -> None:
     user_id = callback.from_user.id
 
-    # Add user answer un redis data.
     async with state.proxy() as data:
-        data[USER_ANSWERS_REDIS_KEY].append(callback.data)
-    # Clear last inline keyboard.
-    await clear_last_ikb(user_id=user_id, state=state)
-    # Call next question.
-    await GamesCreator().questions_creator(user_id=callback.from_user.id, state=state)
+        # Add user answer in redis data.
+        data[USER_ANSWERS_REDIS_KEY].append((data[GAME_QUESTION_REDIS_KEY], callback.data))
+        # Clear last inline keyboard.
+        await clear_last_ikb(user_id=user_id, state=state)
+        # Check whether this question is the last one.
+        if data[GAME_QUESTION_NUMBER_REDIS_KEY] == len(GAMES_DATA[data[GAME_NAME_REDIS_KEY]]) - 1:
+            #Set finish_question state.
+            await GamesStatesGroup.finish_question.set()
+        else:
+            # Let's move on to the next question.
+            data[GAME_QUESTION_NUMBER_REDIS_KEY] += 1
+            # Call questions creator.
+            await call_questions_creator(user_id=user_id, state=state)

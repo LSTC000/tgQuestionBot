e39668e10ebf6d4a1a72bd54476a75a2f5f59fe1
Ivan <razgonyaev_74@mail.ru>
2023-06-26 18:03:55 +0700

    update naruto_config.py

--- data/config/questions/games/naruto_config.py
+++ data/config/questions/games/naruto_config.py
@@ -1,7 +1,19 @@
-from data.paths import NARUTO_QUESTION_IMAGE_PATH_0
+from data.paths import (
+    NARUTO_QUESTION_IMAGE_PATH_0,
+    NARUTO_QUESTION_IMAGE_PATH_1,
+    NARUTO_QUESTION_IMAGE_PATH_2,
+    NARUTO_QUESTION_IMAGE_PATH_3,
+    NARUTO_QUESTION_IMAGE_PATH_4,
+    NARUTO_QUESTION_IMAGE_PATH_5,
+    NARUTO_QUESTION_IMAGE_PATH_6,
+    NARUTO_QUESTION_IMAGE_PATH_7,
+    NARUTO_QUESTION_IMAGE_PATH_8,
+    NARUTO_QUESTION_IMAGE_PATH_9,
+    NARUTO_QUESTION_IMAGE_PATH_10,
+)
 
 
-NARUTO_NAME = 'Naruto'
+NARUTO_NAME = 'Кто ты из Наруто?'
 NARUTO_DATA = {
     0: {
         'image': {
@@ -9,12 +21,161 @@ NARUTO_DATA = {
             'bytes': None,
             'url': None
         },
-        'question': 'Вопрос 1',
-        'answers': ['A', 'B', 'C', 'D']
+        'question': 'Какой ваш возраст?',
+        'answers': [
+            'Молодой (18-26)',
+            'Подросток(12-18)',
+            'Взрослый (26-40)',
+            'Пожилой (40-...)',
+        ]
     },
     1: {
-        'image': None,
-        'question': 'Вопрос 2',
-        'answers': ['A', 'B', 'C', 'D']
-    }
+        'image': {
+            'path': NARUTO_QUESTION_IMAGE_PATH_1,
+            'bytes': None,
+            'url': None
+        },
+        'question': 'Насколько вы доверяете людям?',
+        'answers': [
+            'Я легко доверяю людям',
+            'Я вообще им не доверяю',
+            'Я не особо доверчив',
+            'Доверяю но проверяю',
+        ]
+    },
+    2: {
+        'image': {
+            'path': NARUTO_QUESTION_IMAGE_PATH_2,
+            'bytes': None,
+            'url': None
+        },
+        'question': 'Как бы вы предпочли бы провести свободное время?',
+        'answers': [
+            'Тренироватся',
+            'Читать',
+            'Бездельничать',
+            'Гулять, развлекатся',
+            'У меня нет свободного времени',
+        ]
+    },
+    3: {
+        'image': {
+            'path': NARUTO_QUESTION_IMAGE_PATH_3,
+            'bytes': None,
+            'url': None
+        },
+        'question': 'Что для вас значит слово "друг"?',
+        'answers': [
+            'Тот с кем всегда можно поболтать',
+            'Тот на кого можно положится',
+            'Для меня это почти как семья',
+            'Для меня это союзник',
+            'Это просто слово',
+        ]
+    },
+    4: {
+        'image': {
+            'path': NARUTO_QUESTION_IMAGE_PATH_4,
+            'bytes': None,
+            'url': None
+        },
+        'question': 'В какой кампании вы бы хотели присутствовать?',
+        'answers': [
+            'Клан Учиха',
+            'Команда №7',
+            'Акацуки',
+            'Анбу',
+            'Ни в одной из них',
+            'Клан Узумаки',
+            'Каге',
+            'Клан Хьюга',
+        ]
+    },
+    5: {
+        'image': {
+            'path': NARUTO_QUESTION_IMAGE_PATH_5,
+            'bytes': None,
+            'url': None
+        },
+        'question': 'Какой тип ведения боя предпочитаете?',
+        'answers': [
+            'Ниндзюцу',
+            'Тайдзюцу',
+            'Гендзюцу',
+        ]
+    },
+    6: {
+        'image': {
+            'path': NARUTO_QUESTION_IMAGE_PATH_6,
+            'bytes': None,
+            'url': None
+        },
+        'question': 'Какой ваш биологический пол?',
+        'answers': [
+            'Мужчина',
+            'Женщина',
+        ]
+    },
+    7: {
+        'image': {
+            'path': NARUTO_QUESTION_IMAGE_PATH_7,
+            'bytes': None,
+            'url': None
+        },
+        'question': 'Чего вы больше боитесь?',
+        'answers': [
+            'Смерти',
+            'Быть бесполезным',
+            'Быть одиноким',
+            'Быть слабым',
+            'Быть забытым',
+        ]
+    },
+    8: {
+        'image': {
+            'path': NARUTO_QUESTION_IMAGE_PATH_8,
+            'bytes': None,
+            'url': None
+        },
+        'question': 'Какое слово вас лучше описывает?',
+        'answers': [
+            'Любовь',
+            'Свобода',
+            'Интеллект',
+            'Жадность',
+            'Рассудительность',
+            'Рассудительность',
+        ]
+    },
+    9: {
+        'image': {
+            'path': NARUTO_QUESTION_IMAGE_PATH_9,
+            'bytes': None,
+            'url': None
+        },
+        'question': 'Что вы делаете когда что-то не выходит?',
+        'answers': [
+            'Брошу дело',
+            'Я обычно справляюсь с первого раза',
+            'Попрошу у кого-нибудь помощи',
+            'Отдохну и снова за работу',
+            'Буду трудится пока не выйдет',
+        ]
+    },
+    10: {
+        'image': {
+            'path': NARUTO_QUESTION_IMAGE_PATH_10,
+            'bytes': None,
+            'url': None
+        },
+        'question': 'Какой характер больше на вас похож?',
+        'answers': [
+            'Замкнутый, бесстрастный',
+            'Непредсказуемый',
+            'Спокойный, позитивный',
+            'Жизнерадостный, общительный ',
+            'Беземоциональный',
+            'Резкий, агрессивный',
+        ]
+    },
 }

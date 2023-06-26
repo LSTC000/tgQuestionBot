8c67321ebbf7c5b55f35fad5c6e82fca4619a8b6
Ivan <razgonyaev_74@mail.ru>
2023-06-26 16:49:22 +0700

    delete process_successful_payment.py

--- handlers/users/payment/__init__.py
+++ handlers/users/payment/__init__.py
@@ -3,12 +3,10 @@ __all__ = ['register_users_payment']
 
 from .payment_clb import payment_clb
 from .payment_msg import payment_msg
-from .process_successful_payment import process_successful_payment
 
 from aiogram import Dispatcher
 
 
 def register_users_payment(dp: Dispatcher):
     dp.register_message_handler(payment_msg)
-    dp.register_message_handler(process_successful_payment)
     dp.register_callback_query_handler(payment_clb)

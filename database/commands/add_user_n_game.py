74b3155c41a1ee09437d66d9a52b79cceee4d829
Ivan <razgonyaev_74@mail.ru>
2023-06-26 23:21:59 +0700

    update add_user_n_game.py

--- database/commands/add_user_n_game.py
+++ database/commands/add_user_n_game.py
@@ -18,8 +18,8 @@ async def add_user_n_game(user_id: int, game_name: str) -> None:
             game_name=game_name,
             opened=0,
             completed=0,
-            like=0,
-            dislikes=0
+            like=False,
+            dislikes=False
         )
         await user_n_game.create()
     except UniqueViolationError:

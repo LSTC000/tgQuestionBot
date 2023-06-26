db9bf95f2f6f893128d2b551fce2e2dd46bbce05
Ivan <razgonyaev_74@mail.ru>
2023-06-26 13:44:42 +0700

    add process_pre_checkout_query.py

--- handlers/users/payment/__init__.py
+++ handlers/users/payment/__init__.py
@@ -3,6 +3,7 @@ __all__ = ['register_users_payment']
 
 from .payment_clb import payment_clb
 from .payment_msg import payment_msg
+from .process_pre_checkout_query import process_pre_checkout_query
 
 from aiogram import Dispatcher
 
@@ -10,3 +11,4 @@ from aiogram import Dispatcher
 def register_users_payment(dp: Dispatcher):
     dp.register_message_handler(payment_msg)
     dp.register_callback_query_handler(payment_clb)
+    dp.register_pre_checkout_query_handler(process_pre_checkout_query)
---
+++ handlers/users/payment/process_pre_checkout_query.py
@@ -0,0 +1,8 @@
+from loader import dp, bot
+
+from aiogram import types
+
+
+@dp.pre_checkout_query_handler(func=lambda query: True)
+async def process_pre_checkout_query(pre_checkout_query: types.PreCheckoutQuery):
+    await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)

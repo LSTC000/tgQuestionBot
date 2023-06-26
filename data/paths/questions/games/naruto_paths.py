05fe604e3f6d0d240afc4929058e2a1c4c81916c
Ivan <razgonyaev_74@mail.ru>
2023-06-26 18:03:49 +0700

    update naruto_paths.py

--- data/paths/questions/games/naruto_paths.py
+++ data/paths/questions/games/naruto_paths.py
@@ -1,2 +1,12 @@
 # QUESTIONS.
 NARUTO_QUESTION_IMAGE_PATH_0 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\naruto\questions\0.jpg'
+NARUTO_QUESTION_IMAGE_PATH_1 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\naruto\questions\1.jpg'
+NARUTO_QUESTION_IMAGE_PATH_2 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\naruto\questions\2.jpg'
+NARUTO_QUESTION_IMAGE_PATH_3 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\naruto\questions\3.jpg'
+NARUTO_QUESTION_IMAGE_PATH_4 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\naruto\questions\4.jpg'
+NARUTO_QUESTION_IMAGE_PATH_5 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\naruto\questions\5.jpg'
+NARUTO_QUESTION_IMAGE_PATH_6 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\naruto\questions\6.jpg'
+NARUTO_QUESTION_IMAGE_PATH_7 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\naruto\questions\7.jpg'
+NARUTO_QUESTION_IMAGE_PATH_8 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\naruto\questions\8.jpg'
+NARUTO_QUESTION_IMAGE_PATH_9 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\naruto\questions\9.jpg'
+NARUTO_QUESTION_IMAGE_PATH_10 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\naruto\questions\10.jpg'

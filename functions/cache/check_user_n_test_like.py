cef074cd877bdb39dcf6780e9d5535b28864aa6b
Ivan <razgonyaev_74@mail.ru>
2023-06-26 22:58:06 +0700

    add check_user_n_test_like.py

---
+++ functions/cache/check_user_n_test_like.py
@@ -0,0 +1,23 @@
+from loader import users_n_test_cache
+
+from database import check_user_n_test, check_user_n_test_like
+
+
+async def check_user_n_test_like(user_id: int, test_name: str) -> bool:
+    '''
+    :param user_id: Telegram user_id.
+    :param test_name: Test name.
+    :return: True if the like is True, else False.
+    '''
+
+    if user_id in users_n_test_cache.keys():
+        if test_name in users_n_test_cache[user_id].keys():
+            return users_n_test_cache[user_id][test_name]
+        else:
+            user_n_test_like = await check_user_n_test(user_id=user_id, test_name=test_name)
+            users_n_test_cache[user_id][test_name] = user_n_test_like
+            return user_n_test_like
+    else:
+        user_n_test_like = await check_user_n_test_like(user_id=user_id, test_name=test_name)
+        users_n_test_cache[user_id] = {test_name: user_n_test_like}
+        return user_n_test_like

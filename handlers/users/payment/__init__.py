a0a8e25296181be8cb90d41e5b317d8078c355b4
Ivan <razgonyaev_74@mail.ru>
2023-06-26 16:48:50 +0700

    delete process_pre_checkout_query.py

--- handlers/users/payment/__init__.py
+++ handlers/users/payment/__init__.py
@@ -3,7 +3,6 @@ __all__ = ['register_users_payment']
 
 from .payment_clb import payment_clb
 from .payment_msg import payment_msg
-from .process_pre_checkout_query import process_pre_checkout_query
 from .process_successful_payment import process_successful_payment
 
 from aiogram import Dispatcher
@@ -13,4 +12,3 @@ def register_users_payment(dp: Dispatcher):
     dp.register_message_handler(payment_msg)
     dp.register_message_handler(process_successful_payment)
     dp.register_callback_query_handler(payment_clb)
-    dp.register_pre_checkout_query_handler(process_pre_checkout_query)

890e8bc135cb8f91c57308e4eb0486da298d932f
Ivan <razgonyaev_74@mail.ru>
2023-06-26 23:43:01 +0700

    update check_user_n_test_like.py

--- database/commands/check_user_n_test_like.py
+++ database/commands/check_user_n_test_like.py
@@ -5,11 +5,11 @@ async def check_user_n_test_like(user_id: int, test_name: str) -> bool:
     """
     :param user_id: Telegram user id.
     :param test_name: Test name.
-    :return: True if like is 1, else False.
+    :return: True if like is True, else False.
     """
 
     return True if await UsersAndTests.query.where(
         (UsersAndTests.user_id == user_id) &
         (UsersAndTests.test_name == test_name) &
-        (UsersAndTests.like == 1)
+        (UsersAndTests.like == True)
     ).gino.all() else False

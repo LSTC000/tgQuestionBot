4762005a55b5f3788dcc70b5e44b7b10295ecb2a
Ivan <razgonyaev_74@mail.ru>
2023-06-26 13:54:14 +0700

    update process_pre_checkout_query.py

--- handlers/users/payment/process_pre_checkout_query.py
+++ handlers/users/payment/process_pre_checkout_query.py
@@ -3,6 +3,6 @@ from loader import dp, bot
 from aiogram import types
 
 
-@dp.pre_checkout_query_handler(func=lambda query: True)
+@dp.pre_checkout_query_handler(lambda query: True)
 async def process_pre_checkout_query(pre_checkout_query: types.PreCheckoutQuery):
     await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)

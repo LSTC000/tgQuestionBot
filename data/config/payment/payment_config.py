88ed04fbc613ee1e3acf2c9aa8fda8852b219e9c
Ivan <razgonyaev_74@mail.ru>
2023-06-26 16:33:37 +0700

    update payment_config.py

--- data/config/payment/payment_config.py
+++ data/config/payment/payment_config.py
@@ -5,6 +5,7 @@ from dotenv import find_dotenv
 
 load_dotenv(find_dotenv())
 
+YOOMONEY_USER_ID = os.getenv('YOOMONEY_USER_ID')
 PAYMENT_TOKEN = os.getenv('PAYMENT_TOKEN')
 
 PAYMENT_TITLE = 'Поддержка проекта'

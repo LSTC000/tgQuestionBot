8589ee240657947b1fe21ab69b76f7622a91fe8b
Ivan <razgonyaev_74@mail.ru>
2023-06-26 17:14:12 +0700

    update payment_msg.py

--- handlers/users/payment/payment_msg.py
+++ handlers/users/payment/payment_msg.py
@@ -4,14 +4,14 @@ from data.messages import ERROR_ENTER_PAYMENT_AMOUNT_MESSAGE
 
 from data.config import PAYMENT_TOKEN, PAYMENT_TITLE, PAYMENT_DESCRIPTION
 
-from functions import call_main_menu_ikb, check_user_alert_cache
+from functions import call_payment_ikb, call_main_menu_ikb, check_user_alert_cache
 
 from states import MainMenuStatesGroup, PaymentStatesGroup
 
 from aiogram import types
 from aiogram.dispatcher.storage import FSMContext
 
-from yoomoney import Quickpay, Client
+from yoomoney import Quickpay
 
 
 @dp.message_handler(state=PaymentStatesGroup.enter_amount)
@@ -28,11 +28,12 @@ async def payment_msg(message: types.Message, state: FSMContext) -> None:
             paymentType='SB',
             sum=amount
         )
-
+        # Call payment inline menu.
+        await call_payment_ikb(user_id=user_id, redirected_url=quickpay.redirected_url, amount=amount, state=state)
     except ValueError:
         # Inform the user about an error when entering the amount.
         await bot.send_message(chat_id=user_id, text=ERROR_ENTER_PAYMENT_AMOUNT_MESSAGE)
-    # Call main inline menu.
-    await call_main_menu_ikb(user_id=user_id, alert=await check_user_alert_cache(user_id), state=state)
-    # Set main_menu_ikb state.
-    await MainMenuStatesGroup.main_menu_ikb.set()
+        # Call main inline menu.
+        await call_main_menu_ikb(user_id=user_id, alert=await check_user_alert_cache(user_id), state=state)
+        # Set main_menu_ikb state.
+        await MainMenuStatesGroup.main_menu_ikb.set()

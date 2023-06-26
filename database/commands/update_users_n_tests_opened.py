8e3a05074a01822803258fb36c3b51336316fd75
Ivan <razgonyaev_74@mail.ru>
2023-06-26 22:19:42 +0700

    add update_users_n_tests_opened.py

---
+++ database/commands/update_users_n_tests_opened.py
@@ -0,0 +1,12 @@
+from database import UsersAndTests
+
+
+async def update_users_n_tests_opened(user_id: int, test_name: str) -> None:
+    """
+    :param user_id: Telegram user id.
+    :param test_name: Test name.
+    :return: None.
+    """
+
+    await UsersAndTests.update.values(opened=UsersAndTests.opened+1).\
+        where((UsersAndTests.user_id == user_id) & (UsersAndTests.test_name == test_name)).gino.status()

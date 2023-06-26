906ba33783a50bead8eae4a76a71cd146751145d
Ivan <razgonyaev_74@mail.ru>
2023-06-26 13:01:47 +0700

    add payment_msg.py

--- handlers/users/payment/__init__.py
+++ handlers/users/payment/__init__.py
@@ -2,9 +2,11 @@ __all__ = ['register_users_payment']
 
 
 from .payment_clb import payment_clb
+from .payment_msg import payment_msg
 
 from aiogram import Dispatcher
 
 
 def register_users_payment(dp: Dispatcher):
+    dp.register_message_handler(payment_msg)
     dp.register_callback_query_handler(payment_clb)
---
+++ handlers/users/payment/payment_msg.py
@@ -0,0 +1,39 @@
+from loader import dp, bot
+
+from data.messages import ERROR_ENTER_PAYMENT_AMOUNT_MESSAGE
+
+from data.config import PAYMENT_TOKEN
+
+from functions import call_main_menu_ikb, check_user_alert_cache
+
+from states import MainMenuStatesGroup, PaymentStatesGroup
+
+from aiogram import types
+from aiogram.dispatcher.storage import FSMContext
+
+
+@dp.message_handler(state=PaymentStatesGroup.enter_amount)
+async def payment_msg(message: types.Message, state: FSMContext) -> None:
+    user_id = message.from_user.id
+
+    try:
+        amount = int(message.text)
+        # Call the payment menu.
+        await bot.send_invoice(
+            chat_id=user_id,
+            title='Я заголовок',
+            description='Я описание',
+            provider_token=PAYMENT_TOKEN,
+            currency='rub',
+            is_flexible=False,
+            prices=[types.LabeledPrice(label='payment', amount=amount*100)],
+            start_parameter='time-machine-example',
+            payload='some-invoice-payload-for-our-internal-use'
+        )
+    except TypeError:
+        # Inform the user about an error when entering the amount.
+        await bot.send_message(chat_id=user_id, text=ERROR_ENTER_PAYMENT_AMOUNT_MESSAGE)
+        # Call main inline menu.
+        await call_main_menu_ikb(user_id=user_id, alert=await check_user_alert_cache(user_id), state=state)
+        # Set main_menu_ikb state.
+        await MainMenuStatesGroup.main_menu_ikb.set()

fc5d100f67800e51867d33e1a52f986dda78ab78
Ivan <razgonyaev_74@mail.ru>
2023-06-26 23:08:59 +0700

    add update_user_n_game_like_cache.py

--- functions/cache/__init__.py
+++ functions/cache/__init__.py
@@ -5,6 +5,7 @@ __all__ = [
     'check_user_n_test_cache',
     'check_user_n_game_like_cache',
     'check_user_n_test_like_cache',
+    'update_user_n_game_like_cache',
     'add_user_alert_cache',
     'delete_user_alert_cache',
 ]
@@ -19,6 +20,7 @@ from .check_user_info_cache import check_user_info_cache
 # UsersAndGames.
 from .check_user_n_game_cache import check_user_n_game_cache
 from .check_user_n_game_like_cache import check_user_n_game_like_cache
+from .update_user_n_game_like_cache import update_user_n_game_like_cache
 # UsersAndTests.
 from .check_user_n_test_cache import check_user_n_test_cache
 from .check_user_n_test_like_cache import check_user_n_test_like_cache
---
+++ functions/cache/update_user_n_game_like_cache.py
@@ -0,0 +1,18 @@
+from loader import users_n_games_cache
+
+from database import update_users_n_games_like
+
+
+async def update_user_n_game_like_cache(user_id: int, game_name: str, value: bool) -> None:
+    '''
+    :param user_id: Telegram user_id.
+    :param game_name: Game name.
+    :param value: True if like, else False.
+    :return: None.
+    '''
+
+    if user_id in users_n_games_cache.keys():
+        if game_name in users_n_games_cache[user_id].keys():
+            users_n_games_cache[user_id][game_name] = value
+
+    await update_users_n_games_like(user_id=user_id, game_name=game_name, value=value)

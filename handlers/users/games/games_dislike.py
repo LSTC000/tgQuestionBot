6d16c85a79426bd84a93d0a8d6ceb360ff5b4e9b
Ivan <razgonyaev_74@mail.ru>
2023-06-27 14:08:16 +0700

    update games_dislike.py

--- handlers/users/games/games_dislike.py
+++ handlers/users/games/games_dislike.py
@@ -1,12 +1,19 @@
-from loader import dp
+from loader import dp, bot
 
 from data.callbacks import CANCEL_TO_MAIN_MENU_CALLBACK_DATA
 
+from data.messages import SUCCESSFUL_SAVE_GAME_DISLIKE_MESSAGE, ERROR_SAVE_GAME_DISLIKE_MESSAGE
+
 from data.redis import GAME_NAME_REDIS_KEY
 
-from database import update_game_dislikes, update_user_dislikes, update_user_n_game_dislike
+from database import update_game_dislikes, update_user_dislikes
 
-from functions import update_user_n_game_like_cache, check_user_n_game_like_cache
+from functions import (
+    update_user_n_game_like_cache,
+    update_user_n_game_dislike_cache,
+    check_user_n_game_like_cache,
+    check_user_n_game_dislike_cache
+)
 
 from states import GamesStatesGroup
 
@@ -25,22 +32,18 @@ async def games_dislike(callback: types.CallbackQuery, state: FSMContext) -> Non
     async with state.proxy() as data:
         game_name = data[GAME_NAME_REDIS_KEY]
 
-    # Add 1 value to game dislikes.
-    await update_game_dislikes(game_name)
-    # Add 1 value to user dislikes.
-    await update_user_dislikes(game_name)
-    # Set True value to user and game dislike.
-    await update_user_n_game_dislike(
-        user_id=user_id,
-        game_name=game_name,
-        value=True
-    )
+    if not await check_user_n_game_dislike_cache(user_id=user_id, game_name=game_name):
+        # Add 1 value to game dislikes.
+        await update_game_dislikes(game_name)
+        # Add 1 value to user dislikes.
+        await update_user_dislikes(game_name)
+        # Set True value to user and game dislike.
+        await update_user_n_game_dislike_cache(user_id=user_id, game_name=game_name, value=True)
 
-    if await check_user_n_game_like_cache(user_id=user_id, game_name=game_name):
-        # Set False value to user and game like.
-        await update_user_n_game_like_cache(
-            user_id=user_id,
-            game_name=game_name,
-            value=False
-        )
+        if await check_user_n_game_like_cache(user_id=user_id, game_name=game_name):
+            # Set False value to user and game like.
+            await update_user_n_game_like_cache(user_id=user_id, game_name=game_name, value=False)
 
+        await bot.send_message(chat_id=user_id, text=SUCCESSFUL_SAVE_GAME_DISLIKE_MESSAGE)
+    else:
+        await bot.send_message(chat_id=user_id, text=ERROR_SAVE_GAME_DISLIKE_MESSAGE)

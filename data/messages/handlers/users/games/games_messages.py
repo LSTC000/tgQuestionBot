bdb401d39d33fd94255cd09cd1309d495c30c2cc
Ivan <razgonyaev_74@mail.ru>
2023-06-27 14:00:54 +0700

    update games_messages.py

--- data/messages/handlers/users/games/games_messages.py
+++ data/messages/handlers/users/games/games_messages.py
@@ -1,2 +1,4 @@
 ENTER_GAME_REVIEW_MESSAGE = '✏ Введите ваш отзыв о пройденной игре'
 SUCCESSFUL_SAVE_GAME_REVIEW_MESSAGE = '✅ Ваш отзыв был успешно сохранён'
+SUCCESSFUL_SAVE_LIKE_MESSAGE = '👍 Успешно сохранён'
+SUCCESSFUL_SAVE_DISLIKE_MESSAGE = '👎 Успешно сохранён'

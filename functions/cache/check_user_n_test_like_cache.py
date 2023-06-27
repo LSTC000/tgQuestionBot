fd3b1471612a8e1fa82524aada28a385e6488115
Ivan <razgonyaev_74@mail.ru>
2023-06-27 13:47:38 +0700

    update check_user_n_test_like_cache.py

--- functions/cache/check_user_n_test_like_cache.py
+++ functions/cache/check_user_n_test_like_cache.py
@@ -1,6 +1,6 @@
 from loader import users_n_test_cache
 
-from database import check_user_n_test_like
+from database import check_user_n_test_like, check_user_n_test_dislike
 
 
 async def check_user_n_test_like_cache(user_id: int, test_name: str) -> bool:
@@ -12,12 +12,14 @@ async def check_user_n_test_like_cache(user_id: int, test_name: str) -> bool:
 
     if user_id in users_n_test_cache.keys():
         if test_name in users_n_test_cache[user_id].keys():
-            return users_n_test_cache[user_id][test_name]
+            return users_n_test_cache[user_id][test_name][0]
         else:
             user_n_test_like = await check_user_n_test_like(user_id=user_id, test_name=test_name)
-            users_n_test_cache[user_id][test_name] = user_n_test_like
+            user_n_test_dislike = await check_user_n_test_dislike(user_id=user_id, test_name=test_name)
+            users_n_test_cache[user_id][test_name] = [user_n_test_like, user_n_test_dislike]
             return user_n_test_like
     else:
         user_n_test_like = await check_user_n_test_like(user_id=user_id, test_name=test_name)
-        users_n_test_cache[user_id] = {test_name: user_n_test_like}
+        user_n_test_dislike = await check_user_n_test_dislike(user_id=user_id, test_name=test_name)
+        users_n_test_cache[user_id] = {test_name: [user_n_test_like, user_n_test_dislike]}
         return user_n_test_like

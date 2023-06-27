925c75a47446b80a8f3b1bde287bf6a08acdbdad
Ivan <razgonyaev_74@mail.ru>
2023-06-27 11:47:18 +0700

    move call_games_picker and call_tests_picker

--- functions/handlers/__init__.py
+++ functions/handlers/__init__.py
@@ -2,8 +2,6 @@ __all__ = [
     'call_start_ikb',
     'call_main_menu_ikb',
     'call_payment_ikb',
-    'call_games_picker',
-    'call_tests_picker',
     'edit_main_menu_ikb',
     'clear_last_ikb',
     'clear_redis_data',
@@ -14,9 +12,6 @@ __all__ = [
 from .call_start_ikb import call_start_ikb
 from .call_main_menu_ikb import call_main_menu_ikb
 from .call_payment_ikb import call_payment_ikb
-# CALL INLINE PICKERS.
-from .call_games_picker import call_games_picker
-from .call_tests_picker import call_tests_picker
 # CLEARS.
 from .clear_last_ikb import clear_last_ikb
 from .clear_redis_data import clear_redis_data
---
+++ functions/pickers/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['call_games_picker', 'call_tests_picker']
+
+
+from .call_games_picker import call_games_picker
+from .call_tests_picker import call_tests_picker

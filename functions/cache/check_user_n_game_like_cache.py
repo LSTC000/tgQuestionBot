234a471d7622ac90a94afa8961dbda576338d22b
Ivan <razgonyaev_74@mail.ru>
2023-06-27 13:43:15 +0700

    update check_user_n_game_like_cache.py

--- functions/cache/check_user_n_game_like_cache.py
+++ functions/cache/check_user_n_game_like_cache.py
@@ -1,6 +1,6 @@
 from loader import users_n_games_cache
 
-from database import check_user_n_game_like
+from database import check_user_n_game_like, check_user_n_game_dislike
 
 
 async def check_user_n_game_like_cache(user_id: int, game_name: str) -> bool:
@@ -12,12 +12,14 @@ async def check_user_n_game_like_cache(user_id: int, game_name: str) -> bool:
 
     if user_id in users_n_games_cache.keys():
         if game_name in users_n_games_cache[user_id].keys():
-            return users_n_games_cache[user_id][game_name]
+            return users_n_games_cache[user_id][game_name][0]
         else:
             user_n_game_like = await check_user_n_game_like(user_id=user_id, game_name=game_name)
-            users_n_games_cache[user_id][game_name] = user_n_game_like
+            user_n_game_dislike = await check_user_n_game_dislike(user_id=user_id, game_name=game_name)
+            users_n_games_cache[user_id][game_name] = [user_n_game_like, user_n_game_dislike]
             return user_n_game_like
     else:
         user_n_game_like = await check_user_n_game_like(user_id=user_id, game_name=game_name)
-        users_n_games_cache[user_id] = {game_name: user_n_game_like}
+        user_n_game_dislike = await check_user_n_game_dislike(user_id=user_id, game_name=game_name)
+        users_n_games_cache[user_id] = {game_name: [user_n_game_like, user_n_game_dislike]}
         return user_n_game_like

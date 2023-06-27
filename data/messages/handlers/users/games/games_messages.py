f1e809c50df60c48b473e1f1f2ff5af4c58f1f27
Ivan <razgonyaev_74@mail.ru>
2023-06-27 14:03:49 +0700

    update games_messages.py

--- data/messages/handlers/users/games/games_messages.py
+++ data/messages/handlers/users/games/games_messages.py
@@ -1,4 +1,6 @@
 ENTER_GAME_REVIEW_MESSAGE = '✏ Введите ваш отзыв о пройденной игре'
 SUCCESSFUL_SAVE_GAME_REVIEW_MESSAGE = '✅ Ваш отзыв был успешно сохранён'
-SUCCESSFUL_SAVE_LIKE_MESSAGE = '👍 Успешно сохранён'
-SUCCESSFUL_SAVE_DISLIKE_MESSAGE = '👎 Успешно сохранён'
+SUCCESSFUL_SAVE_GAME_LIKE_MESSAGE = '👍 Успешно сохранён'
+ERROR_SAVE_GAME_LIKE_MESSAGE = '❗ Вы уже поставили 👍 этой игре'
+SUCCESSFUL_SAVE_GAME_DISLIKE_MESSAGE = '👎 Успешно сохранён'
+ERROR_SAVE_GAME_DISLIKE_MESSAGE = '❗ Вы уже поставили 👎 этой игре'

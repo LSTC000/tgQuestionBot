59ae005ddfab8f8b917f4da378c63d6bdfc6a3a4
Ivan <razgonyaev_74@mail.ru>
2023-06-27 14:25:07 +0700

    update GamesStatesGroup

--- states/games.py
+++ states/games.py
@@ -4,4 +4,4 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 class GamesStatesGroup(StatesGroup):
     game_question = State()
     finish_question = State()
-    enter_review = State
+    enter_review = State()

cd425a4c87e0d8db93c0181f2766e8f9c767c713
Ivan <razgonyaev_74@mail.ru>
2023-06-27 13:52:56 +0700

    update update_user_n_test_dislike.py

--- database/commands/update_user_n_test_dislike.py
+++ database/commands/update_user_n_test_dislike.py
@@ -1,13 +1,13 @@
 from database import UsersAndTests
 
 
-async def update_user_n_test_dislike(user_id: int, game_name: str, value: bool) -> None:
+async def update_user_n_test_dislike(user_id: int, test_name: str, value: bool) -> None:
     """
     :param user_id: Telegram user id.
-    :param game_name: Game name.
+    :param test_name: Game name.
     :param value: True or False.
     :return: None.
     """
 
     await UsersAndTests.update.values(dislike=value).\
-        where((UsersAndTests.user_id == user_id) & (UsersAndTests.game_name == game_name)).gino.status()
+        where((UsersAndTests.user_id == user_id) & (UsersAndTests.test_name == test_name)).gino.status()

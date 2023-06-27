3b28e11cd3b247a6b0d13a0277cba6b7d176cf0e
Ivan <razgonyaev_74@mail.ru>
2023-06-27 13:07:50 +0700

    add games_dislike.py

--- handlers/users/games/__init__.py
+++ handlers/users/games/__init__.py
@@ -3,6 +3,8 @@ __all__ = ['register_users_games']
 
 from .games_creator import games_creator
 from .games_like import games_like
+from .games_dislike import games_dislike
+
 
 from aiogram import Dispatcher
 
@@ -10,3 +12,4 @@ from aiogram import Dispatcher
 def register_users_games(dp: Dispatcher):
     dp.register_callback_query_handler(games_creator)
     dp.register_callback_query_handler(games_like)
+    dp.register_callback_query_handler(games_dislike)

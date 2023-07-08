52c4040bad5196880b9344fb44e92aa462c8e350
Ivan <razgonyaev_74@mail.ru>
2023-07-09 01:21:29 +0700

    update keyboards_messages.py

--- data/messages/keyboards/inline_keyboards/keyboards_messages.py
+++ data/messages/keyboards/inline_keyboards/keyboards_messages.py
@@ -8,6 +8,10 @@ TESTS_PICKER_IKB_MESSAGE = '📕 Выберите тест'
 USER_ALERT_ON_IKB_MESSAGE = '🔔 Включить уведомления'
 USER_ALERT_OFF_IKB_MESSAGE = '🔕 Отключить уведомления'
 PAYMENT_IKB_MESSAGE = '💰 Поддержать проект'
+# ADMIN MENU.
+ALERT_FOR_USERS_IKB_MESSAGE = '🗞️ Отправить объявление'
+CONFIRM_ALERT_FOR_USERS_IKB_MESSAGE = '✅ Да'
+CANCEL_ALERT_FOR_USERS_IKB_MESSAGE = '❌ Нет'
 # PAYMENT.
 PAYMENT_URL_IKB_MESSAGE = 'Оплатить'
 # FINISH QUESTION.

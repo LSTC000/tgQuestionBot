65c885686a8eeebdf5af5eb037563b3e6eb88de3
Ivan <razgonyaev_74@mail.ru>
2023-07-09 01:58:30 +0700

    add call_admin_menu_ikb.py

--- functions/handlers/__init__.py
+++ functions/handlers/__init__.py
@@ -2,6 +2,7 @@ __all__ = [
     'call_start_ikb',
     'call_main_menu_ikb',
     'call_payment_ikb',
+    'call_admin_menu_ikb',
     'edit_main_menu_ikb',
     'clear_last_ikb',
     'clear_redis_data',
@@ -12,6 +13,7 @@ __all__ = [
 from .call_start_ikb import call_start_ikb
 from .call_main_menu_ikb import call_main_menu_ikb
 from .call_payment_ikb import call_payment_ikb
+from .call_admin_menu_ikb import call_admin_menu_ikb
 # CLEARS.
 from .clear_last_ikb import clear_last_ikb
 from .clear_redis_data import clear_redis_data

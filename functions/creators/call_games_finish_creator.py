dad6817683e8c129dde9dac45706d81d88c14b79
Ivan <razgonyaev_74@mail.ru>
2023-07-08 15:41:18 +0700

    update call_games_finish_creator.py

--- functions/creators/call_games_finish_creator.py
+++ functions/creators/call_games_finish_creator.py
@@ -17,12 +17,8 @@ async def call_games_finish_creator(user_id: int, state: FSMContext) -> None:
     """
 
     async with state.proxy() as data:
-        # Wait Chat GPT response message.
-        msg = await bot.send_message(chat_id=user_id, text=WAIT_CHAT_GPT_RESPONSE_MESSAGE)
-        # Get Chat GPT answer and finish inline keyboard.
+        # Get answer and finish inline keyboard.
         answer, ikb = await GamesCreator().finish_creator(state)
-        # Delete wait Chat GPT response message.
-        await bot.delete_message(chat_id=user_id, message_id=msg.message_id)
         # Call finish creator.
         msg = await bot.send_message(chat_id=user_id, text=answer, reply_markup=ikb)
         # Remember id of the last inline keyboard.

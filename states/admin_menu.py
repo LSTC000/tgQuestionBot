9e48f86e45af453842471e10c5caf9359c99fcd2
Ivan <razgonyaev_74@mail.ru>
2023-07-09 02:02:23 +0700

    rename confirm_for_users -> confirm_alert_for_users

--- states/admin_menu.py
+++ states/admin_menu.py
@@ -4,4 +4,4 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 class AdminMenuStatesGroup(StatesGroup):
     admin_menu = State()
     alert_for_users = State()
-    confirm_for_users = State()
+    confirm_alert_for_users = State()

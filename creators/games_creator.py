c9798faf3b5822b97e9eeef2e823cc7f1c8a2b6f
Ivan <razgonyaev_74@mail.ru>
2023-07-08 16:40:31 +0700

    update games_creator.py

--- creators/games_creator.py
+++ creators/games_creator.py
@@ -63,8 +63,23 @@ class GamesCreator:
     async def finish_creator(self, state: FSMContext) -> tuple:
         """
         :param state: FSMContext.
-        :return: Answer and finish inline keyboard.
+        :return: Answer, image answer if it exists and finish inline keyboard.
         """
+
+        async with state.proxy() as data:
+            game_data = GAMES_DATA[data[GAME_NAME_REDIS_KEY]][1]
+            answer = AnswerFinder().best_weight(data[USER_ANSWERS_REDIS_KEY])
+
+        image = game_data[answer]['image']
+
+        if image is not None:
+            for key in image.keys():
+                if image[key] is not None:
+                    if key == 'path' and os.path.exists(image[key]):
+                        image = InputFile(path_or_bytesio=image[key])
+                    else:
+                        image = image[key]
+                    break
 
         ikb = InlineKeyboardMarkup(row_width=ROW_WIDTH)
 
@@ -77,7 +92,4 @@ class GamesCreator:
             InlineKeyboardButton(text=CANCEL_TO_MAIN_MENU_IKB_MESSAGE, callback_data=CANCEL_TO_MAIN_MENU_CALLBACK_DATA)
         )
 
-        async with state.proxy() as data:
-            answer = AnswerFinder().best_weight(data[USER_ANSWERS_REDIS_KEY])
-
-        return answer, ikb
+        return image, answer, ikb

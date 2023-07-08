4f848c2c75396f82b7440744505b2deabc91cbb0
Ivan <razgonyaev_74@mail.ru>
2023-07-09 01:46:40 +0700

    update redis_keys.py

--- data/redis/redis_keys.py
+++ data/redis/redis_keys.py
@@ -8,3 +8,5 @@ GAME_QUESTION_NUMBER_REDIS_KEY = 'game_question_number'
 TEST_NAME_REDIS_KEY = 'test_name'
 TEST_QUESTION_NUMBER_REDIS_KEY = 'test_question_number'
 USER_ANSWERS_REDIS_KEY = 'user_answers'
+# ADMIN MENU.
+ALERT_FOR_USERS_REDIS_KEY = 'alert_for_users'

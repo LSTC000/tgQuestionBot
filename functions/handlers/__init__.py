4e7ff81dbf8b1214246822427b53d9a576aef402
Ivan <razgonyaev_74@mail.ru>
2023-07-09 02:06:34 +0700

    add call_confirm_alert_for_users_menu_ikb.py

--- functions/handlers/__init__.py
+++ functions/handlers/__init__.py
@@ -6,6 +6,7 @@ __all__ = [
     'edit_main_menu_ikb',
     'clear_last_ikb',
     'clear_redis_data',
+    'call_confirm_alert_for_users_menu_ikb'
 ]
 
 
@@ -14,6 +15,7 @@ from .call_start_ikb import call_start_ikb
 from .call_main_menu_ikb import call_main_menu_ikb
 from .call_payment_ikb import call_payment_ikb
 from .call_admin_menu_ikb import call_admin_menu_ikb
+from .call_confirm_alert_for_users_menu_ikb import call_confirm_alert_for_users_menu_ikb
 # CLEARS.
 from .clear_last_ikb import clear_last_ikb
 from .clear_redis_data import clear_redis_data

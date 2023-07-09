eb6ac87efcfa1e79b85b15ab24fe4c903982d123
Ivan <razgonyaev_74@mail.ru>
2023-07-09 16:30:07 +0700

    update games_review_msg.py

--- handlers/users/games/games_review_msg.py
+++ handlers/users/games/games_review_msg.py
@@ -1,6 +1,8 @@
 from loader import dp, bot
 
-from data.messages import SUCCESSFUL_SAVE_GAME_REVIEW_MESSAGE
+from data.config import MAX_REVIEW_CHARS
+
+from data.messages import SUCCESSFUL_SAVE_GAME_REVIEW_MESSAGE, ERROR_SAVE_GAME_REVIEW_MESSAGE
 
 from data.redis import GAME_NAME_REDIS_KEY
 
@@ -15,13 +17,19 @@ from aiogram.dispatcher.storage import FSMContext
 @dp.message_handler(state=GamesStatesGroup.enter_review)
 async def games_review_msg(message: types.Message, state: FSMContext) -> None:
     user_id = message.from_user.id
+    text = message.text
 
-    async with state.proxy() as data:
-        game_name = data[GAME_NAME_REDIS_KEY]
+    # Check review length.
+    if len(text) <= MAX_REVIEW_CHARS:
+        async with state.proxy() as data:
+            game_name = data[GAME_NAME_REDIS_KEY]
+        # Add review about the game in database.
+        await add_user_review(user_id=user_id, target='game', target_name=game_name, review=text)
+        # Inform the user about successful save review.
+        await bot.send_message(chat_id=user_id, text=SUCCESSFUL_SAVE_GAME_REVIEW_MESSAGE)
+    else:
+        # Inform the user about error save review.
+        await bot.send_message(chat_id=user_id, text=ERROR_SAVE_GAME_REVIEW_MESSAGE)
 
-    # Add review about the game in database.
-    await add_user_review(user_id=user_id, target='game', target_name=game_name, review=message.text)
-    # Inform the user about successful save review.
-    await bot.send_message(chat_id=user_id, text=SUCCESSFUL_SAVE_GAME_REVIEW_MESSAGE)
     # Set finish_question state.
     await GamesStatesGroup.finish_question.set()

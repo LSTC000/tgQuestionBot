9a1e0c5c1d9fe3f2e703c31351ce8ea3eef45e7f
Ivan <razgonyaev_74@mail.ru>
2023-07-09 13:25:01 +0700

    update payment_msg.py

--- handlers/users/payment/payment_msg.py
+++ handlers/users/payment/payment_msg.py
@@ -20,16 +20,25 @@ async def payment_msg(message: types.Message, state: FSMContext) -> None:
 
     try:
         amount = int(message.text)
-        # Call the payment menu.
-        quickpay = Quickpay(
-            receiver=RECEIVER,
-            quickpay_form=QUICKPAY_FORM,
-            targets=TARGETS,
-            paymentType=PAYMENT_TYPE,
-            sum=amount
-        )
-        # Call payment inline menu.
-        await call_payment_ikb(user_id=user_id, redirected_url=quickpay.redirected_url, amount=amount, state=state)
+
+        if amount > 0:
+            # Call the payment menu.
+            quickpay = Quickpay(
+                receiver=RECEIVER,
+                quickpay_form=QUICKPAY_FORM,
+                targets=TARGETS,
+                paymentType=PAYMENT_TYPE,
+                sum=amount
+            )
+            # Call payment inline menu.
+            await call_payment_ikb(user_id=user_id, redirected_url=quickpay.redirected_url, amount=amount, state=state)
+        else:
+            # Inform the user about an error when entering the amount.
+            await bot.send_message(chat_id=user_id, text=ERROR_ENTER_PAYMENT_AMOUNT_MESSAGE)
+            # Call main inline menu.
+            await call_main_menu_ikb(user_id=user_id, alert=await check_user_alert_cache(user_id), state=state)
+            # Set main_menu_ikb state.
+            await MainMenuStatesGroup.main_menu_ikb.set()
     except ValueError:
         # Inform the user about an error when entering the amount.
         await bot.send_message(chat_id=user_id, text=ERROR_ENTER_PAYMENT_AMOUNT_MESSAGE)

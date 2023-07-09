a6fa5855e691fe1971f77d9166dadbb0e2e1e93f
Ivan <razgonyaev_74@mail.ru>
2023-07-09 16:40:44 +0700

    update answer_finder.py

--- utils/answer_finder.py
+++ utils/answer_finder.py
@@ -4,3 +4,6 @@ class AnswerFinder:
 
     def best_weight(self, answers: dict) -> int:
         return int(max(answers, key=lambda key: answers.get(key)))
+
+    def mean_score(self, correct_count: int, all_count: int) -> float:
+        return round(correct_count / all_count, 2)

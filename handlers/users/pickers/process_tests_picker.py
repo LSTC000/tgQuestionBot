3df6665bdce137ed96b9c4a787b15d55d8a1874e
Ivan <razgonyaev_74@mail.ru>
2023-07-10 18:00:20 +0700

    add process_tests_picker.py

--- handlers/users/pickers/process_tests_picker.py
+++ handlers/users/pickers/process_tests_picker.py
@@ -1,20 +1,21 @@
-from loader import dp, bot
+from loader import dp
 
 from data.callbacks import CANCEL_TO_MAIN_MENU_CALLBACK_DATA
+
+from data.redis import TEST_NAME_REDIS_KEY, TEST_QUESTION_NUMBER_REDIS_KEY, USER_ANSWERS_REDIS_KEY
 
 from database import update_test_attempts, update_user_opened_tests, update_user_n_test_opened, add_user_n_test
 
 from functions import (
     clear_last_ikb,
     clear_redis_data,
-    call_main_menu_ikb,
-    check_user_alert_cache,
-    check_user_n_test_cache
+    call_tests_questions_creator,
+    check_user_n_test_cache,
 )
 
 from pickers import TestsPicker
 
-from states import MainMenuStatesGroup, PickersStatesGroup
+from states import PickersStatesGroup, TestsStatesGroup
 
 from aiogram import types
 from aiogram.dispatcher.storage import FSMContext
@@ -31,21 +32,25 @@ async def process_tests_picker(callback: types.CallbackQuery, state: FSMContext)
     # Check whether the user has made his choice.
     if pick:
         user_id = callback.from_user.id
-        await bot.send_message(chat_id=user_id, text=f'Your choice: {test_name}')
+        # Clear redis data.
+        await clear_redis_data(state)
+        # Add test name, point on first question and user answers in redis data.
+        async with state.proxy() as data:
+            data[TEST_NAME_REDIS_KEY] = test_name
+            data[TEST_QUESTION_NUMBER_REDIS_KEY] = 0
+            data[USER_ANSWERS_REDIS_KEY] = {}
         # Add 1 value for the test attempts.
         await update_test_attempts(test_name)
         # Add 1 value for the user opened tests.
         await update_user_opened_tests(user_id)
-        # Check user and game in database and cache.
+        # Check user and test in database and cache.
         if not await check_user_n_test_cache(user_id=user_id, test_name=test_name):
             await add_user_n_test(user_id=user_id, test_name=test_name)
-        # Add 1 value for the user and game opened.
+        # Add 1 value for the user and test opened.
         await update_user_n_test_opened(user_id=user_id, test_name=test_name)
         # Clear last inline keyboard.
         await clear_last_ikb(user_id=user_id, state=state)
-        # Clear redis data.
-        await clear_redis_data(state)
-        # Call main inline menu.
-        await call_main_menu_ikb(user_id=user_id, alert=await check_user_alert_cache(user_id), state=state)
-        # Set main_menu_ikb state.
-        await MainMenuStatesGroup.main_menu_ikb.set()
+        # Call a first game question.
+        await call_tests_questions_creator(user_id=user_id, state=state)
+        # Set game_question state.
+        await TestsStatesGroup.test_question.set()

63efdbde5c4225011148b6b777389a9db268549b
Ivan <razgonyaev_74@mail.ru>
2023-07-10 20:37:34 +0700

    update avengers_paths.py

--- data/paths/questions/games/avengers_paths.py
+++ data/paths/questions/games/avengers_paths.py
@@ -1,2 +1,13 @@
 # QUESTIONS.
+AVENGERS_QUESTION_IMAGE_PATH_0 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\avengers\questions\0.jpg'
 # ANSWERS
+AVENGERS_RESULT_IMAGE_PATH_0 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\avengers\results\0.jpg'
+AVENGERS_RESULT_IMAGE_PATH_1 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\avengers\results\1.jpg'
+AVENGERS_RESULT_IMAGE_PATH_2 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\avengers\results\2.jpg'
+AVENGERS_RESULT_IMAGE_PATH_3 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\avengers\results\3.jpg'
+AVENGERS_RESULT_IMAGE_PATH_4 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\avengers\results\4.jpg'
+AVENGERS_RESULT_IMAGE_PATH_5 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\avengers\results\5.jpg'
+AVENGERS_RESULT_IMAGE_PATH_6 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\avengers\results\6.jpg'
+AVENGERS_RESULT_IMAGE_PATH_7 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\avengers\results\7.jpg'
+AVENGERS_RESULT_IMAGE_PATH_8 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\avengers\results\8.jpg'
+AVENGERS_RESULT_IMAGE_PATH_9 = r'C:\Games\tgBots\tgQuestionBot\data\images\games\avengers\results\9.jpg'

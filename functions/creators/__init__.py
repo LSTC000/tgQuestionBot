3fe25956ae977fb861efec53580fa1a836ec95b6
Ivan <razgonyaev_74@mail.ru>
2023-07-10 17:59:17 +0700

    add call_tests_questions_creator.py

--- functions/creators/__init__.py
+++ functions/creators/__init__.py
@@ -1,8 +1,10 @@
 __all__ = [
     'call_games_questions_creator',
     'call_games_finish_creator',
+    'call_tests_questions_creator'
 ]
 
 
 from .call_games_questions_creator import call_games_questions_creator
 from .call_games_finish_creator import call_games_finish_creator
+from .call_tests_questions_creator import call_tests_questions_creator
